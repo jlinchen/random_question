217eb9b6e42d8b797d209d811e9645e3d039b997
Jie Lin Chen <jlchen@virtualq.io>
2022-04-06 17:23:12 +0200

    add functionality

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -9,6 +9,16 @@ class QuestionsController < ApplicationController
     @question = Question.find(Question.pluck(:id).sample)
   end
 
+  def only_would_you
+    @question = Question.find(Question.where(would_you: true).pluck(:id).sample)
+    render 'random'
+  end
+
+  def no_would_you
+    @question = Question.find(Question.where(would_you: false).pluck(:id).sample)
+    render 'random'
+  end
+
   private
 
   def questions_count
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'questions#index'
   get 'random', to: 'questions#random'
+  get 'only_would_you', to: 'questions#only_would_you'
+  get 'no_would_you', to: 'questions#no_would_you'
 end
