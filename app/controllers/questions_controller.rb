9baee225c0daf60a2c3b88ea327a733743fbc476
Jie Lin Chen <jlchen@virtualq.io>
2022-04-06 17:06:15 +0200

    frontend ready!

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,9 +1,17 @@
 # frozen_string_literal: true
 
 class QuestionsController < ApplicationController
+  before_action :questions_count
+
   def index; end
 
   def random
     @question = Question.find(Question.pluck(:id).sample)
+  end
+
+  private
+
+  def questions_count
+    @questions_count ||= Question.count
   end
 end
